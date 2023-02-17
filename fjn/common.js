78763c3f91f23bdfd8c119324f9464ea0af27177
Frederico Leonardo <frederico@gomo.digital>
2023-02-17 12:49:59 +0000

    ..

--- fjn/common.js
+++ fjn/common.js
@@ -73,23 +73,16 @@ $(document).ready(function () {
 	});
 
 	// Add feature classes to article list items
-	const articleCards = $('.article-list-collection .article-card');
-	// articleCards.each(function () {
-	// 	const articleFeaturedItem = $(this).find('.article-featured-item');
-	// 	if (articleFeaturedItem.val() === 'true') {
-	// 		const articleFeaturedItemOrder = $(this).find('.article-featured-item-order').val();
-	// 		const articleListItem = $(this).closest('.article-list-item');
-	// 		articleListItem.addClass(`feature-${articleFeaturedItemOrder}`);
-	// 	}
-	// });
 	$('.article-card').each(function () {
 		const articleFeaturedItem = $(this).find('.article-featured-item').val() === 'true';
 		const articleFeaturedItemOrder = parseInt($(this).find('.article-featured-item-order').val());
-		console.log(articleFeaturedItem, articleFeaturedItemOrder);
 		if (articleFeaturedItem) {
-			$(this).closest('.article-list-item').addClass(`feature-${articleFeaturedItemOrder}`);
+			$('.article-list-item').each(function() {
+				if ($(this).index() === (articleFeaturedItemOrder - 1)) {
+					$(this).addClass(`feature-${articleFeaturedItemOrder}`);
+				}
+			});
 		}
 	});
 	
-
 });
